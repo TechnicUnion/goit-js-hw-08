2bce22a943f8b832dfa3f0d9f26ec07a7de8f05c
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-09-26 23:52:44 +0300

    adds task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryItemsList = createGalleryList(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryItemsList);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryList(galleryItems) {
+  return galleryItems
+    .map(({ preview, original }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="Image description" />
+</a>`;
+    })
+    .join('');
+}
