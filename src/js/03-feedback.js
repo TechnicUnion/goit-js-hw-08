d36715c67854a2fd2e65067352b621319b47dfdb
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2022-10-03 00:33:35 +0300

    adds task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+checksLocalStorage();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function onFormInput(evt) {
+  const feedback = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedback));
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function checksLocalStorage() {
+  const saveFeedback = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (saveFeedback) {
+    email.value = saveFeedback.email;
+    message.value = saveFeedback.message;
+  }
+}
